f2d2268e0466de3f0dc0ba359179c1d89df98439
gyeongseok.seo <gyeongseok.seo@samsung.com>
2012-08-09 18:35:08 +0900

    [Title] Added no.de server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  res.sendfile( __dirname + '/index.html' )
+});
+
+app.listen(8080);
+
+console.log('> http server has started on port 8080');
