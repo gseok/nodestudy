0c6de97b990dd9001bd2e589cfa2be221cf937c2
gyeongseok.seo <gyeongseok.seo@samsung.com>
2012-08-09 18:52:07 +0900

    [Title] Fixed index.html position.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.sendfile( __dirname + '/index.html' )
+  res.sendfile( __dirname + 'helloworld/bin/index.html' );
 });
 
 app.listen(8080);
