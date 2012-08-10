bf5e47d4ae802254eacfc911434ed18f78b047ab
gyeongseok.seo <gyeongseok.seo@samsung.com>
2012-08-10 09:38:09 +0900

    [Title] server.js test in no.de

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end( __dirname + 'Hello World\n');
 }).listen(80);
 
 console.log('Server running');
 
-/*
+
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res){
+app.get('/a', function(req, res){
 	console.log( __dirname );
 	res.sendfile( __dirname + '/helloworld/bin/index.html' );
 });
 
-app.listen(8080);
+app.listen(80);
 
 console.log('> http server has started on port 8080');
-*/
