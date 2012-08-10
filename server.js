e80fb47ec21e185d7296d5bb8a583d6f56383a1f
gyeongseok.seo <gyeongseok.seo@samsung.com>
2012-08-10 09:34:21 +0900

    [Title] test default node.js code

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+}).listen(80);
+
+console.log('Server running');
+
+/*
 var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.sendfile( __dirname + '/helloworld/bin/index.html' );
+	console.log( __dirname );
+	res.sendfile( __dirname + '/helloworld/bin/index.html' );
 });
 
 app.listen(8080);
 
 console.log('> http server has started on port 8080');
+*/
